e79abc5cc6805bd5b5185a27c0d16388dcf28f4a
Kishan_Gupta <guptakishan428@gmail.com>
2024-09-27 22:48:48 +0530

    hero-section

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import React from "react";
+
+const Button = ({ icon, children, href, containerClassName }) => {
+  return href ? (
+    <a
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName
+      )}
+    ></a>
+  ) : (
+    <button
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName
+      )}
+    >
+      Text
+    </button>
+  );
+};
+
+export default Button;
